616e1a1eac0fbe99700cbea2da67270faa7da260
SBunnDev <87293052+SBunnDev@users.noreply.github.com>
2022-11-19 23:45:58 -0500

    Flipcard project with API tie in

---
+++ coding-games/src/Flashcard.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useRef } from 'react'
+
+export default function Flashcard({ flashcard }) {
+  const [flip, setFlip] = useState( false )
+  const [height, setHeight] = useState('initial')
+
+  const frontEl = useRef()
+  const backEl = useRef()
+
+  // gets current front and back heights of card rectangles, not allowed to collapse below 100 pixels.
+  function setMaxHeight() {
+    const frontHeight = frontEl.current.getBoundingClientRect().height
+    const backHeight = backEl.current.getBoundingClientRect().height
+    setHeight(Math.max(frontHeight,backHeight, 100))
+  }
+
+  // sets height of flashcards based on text area size, then clears on re-render with removeEventListner to adjust to resize of window.
+  useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
+  useEffect(() => {
+    window.addEventListener('resize', setMaxHeight)
+    return () => window.removeEventListener('resize', setMaxHeight)
+  })
+
+  // Below: Ternary flip operator, forced height inline-styling with variable to allow for easier dynamic resize, element references to front and back with El. NOTE: React gets angry if you map without a key... even if it is redundant a bit.
+  return (
+    <div 
+      className={`card ${flip ? 'flip' : ''}`}
+      style={{ height: height}}
+      onClick={()=> setFlip(!flip)}
+    >
+      <div className='front' ref={frontEl}>
+        {flashcard.question}
+        <div className='flashcard-options'>
+          {flashcard.options.map(option => {
+            return <div className='flashcard-option' key={option}>{option}</div>
+          })}
+        </div>
+      </div>
+      <div className='back' ref={backEl}>
+        {flashcard.answer}
+      </div>
+    </div>
+  )
+}
